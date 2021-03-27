304f86a7197464000b57d558088961b25675200b
nCodeIT <69150716+ncodeit-io@users.noreply.github.com>
2021-03-27 22:03:40 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello NCD feature branch2 World!"
+    return "Hello NCD feature branch3 World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
